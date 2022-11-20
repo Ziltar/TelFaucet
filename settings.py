b3b7911351592a37e2b5f6a494810ff664af5ba9
Ziltar <juri.hopr@gmail.com>
2022-11-20 16:31:32 +0100

    settings

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 TG_BOT_KEY = ""
 WALLET_ADDR = ""
 WALLET_PK = ""
-TOKEN_WHITELIST = ""
+RPC_URL = "https://rpc.gnosischain.com"
+TOKEN_WHITELIST = ["0x43d13d7b83607f14335cf2cb75e87da369d056c7", "0x978d91ddfdb0c6eacc22a258fe498957a79c5f4c", "0xd057604a14982fe8d88c5fc25aac3267ea142a08", "0xd4fdec44db9d44b8f2b6d529620f9c0c7066a2c1"]
+DATABASE_FILE = "faucet.db"
